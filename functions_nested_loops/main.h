95bf1c680a1cf24518afc4a8bc28d9893379b2c3
lorecarreno <rodelugaliza@gmail.com>
2023-02-17 05:23:36 -0800

    2-print_alphabet_x10.c

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -3,7 +3,7 @@
 
 int _putchar(char);
 void print_alphabet(void);
-void print_alphabeth_x10(void);
+void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
@@ -15,4 +15,4 @@ int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
 
-#endif
+#endif 
