e5a8b9b356580ff2be818816e2162c4594389a45
lorecarreno <rodelugaliza@gmail.com>
2023-02-24 11:42:43 -0800

    more_functions_nested_loops

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -7,15 +7,20 @@
 
 void print_square(int size)
 {
-	int rows, columns;
+	int i, j;
 
 	if (size <= 0)
-		_putchar('\n');
-
-	for (rows = size; rows > 0; rows--)
 	{
-	for (columns = size ; columns > 0; columns--)
-		_putchar('#');
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
 		_putchar('\n');
 	}
 }
