635e49d85daa1ec1a986f71683bfb5d663d7a39b
lorecarreno <rodelugaliza@gmail.com>
2023-02-24 07:55:07 -0800

    more_functions_nested_loops

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_square - Print a traight line in the therminal.
+ * @size: square size.
+ */
+
+void print_square(int size)
+{
+	int rows, columns;
+
+	if(size <= 0)
+		_putchar('\n');
+
+	for (rows = size; rows > 0; rows--)
+	{
+		for
+	(columns = size ; columns > 0; columns--)
+		_putchar('#');
+		_putchar('\n');
+	}
+}
