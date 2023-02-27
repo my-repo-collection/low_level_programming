03bd825a64354d2986d54a6175ce86674f228eb8
lorecarreno <rodelugaliza@gmail.com>
2023-02-27 15:35:14 -0800

    pointers_arrays_strings

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,22 +10,22 @@ int _atoi(char *s)
 	int sign = 1;
 	int digit;
 
-    while (*s)
-    {
-        if (*s == '-')
-            sign *= -1;
-        else if (*s >= '0' && *s <= '9')
-        {
-            digit = *s - '0';
-            result = result * 10 + digit;
-        }
-        else if (result > 0)
-        {
-            break;
-        }
+	while (*s)
+	{
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+		{
+			digit = *s - '0';
+			result = result * 10 + digit;
+		}
+		else if (result > 0)
+		{
+			break;
+		}
 
-        s++;
-    }
+		s++;
+	}
 
-    return result * sign;
+	return (result * sign);
 }
