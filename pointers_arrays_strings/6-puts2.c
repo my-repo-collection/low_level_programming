d656db73b52a8de15c746fdd26ede848bb11655d
lorecarreno <rodelugaliza@gmail.com>
2023-02-27 16:31:01 -0800

    pointers_arrays_strings

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * puts2 - Prints every other character of a string,
- * starting with the first character, followed
- * by a new line.
+ * puts2 - Prints every other character of
+ * a string, starting with the first character,
+ * followed by a new line.
  *
  * @str: The string to print.
  */
--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -2,30 +2,22 @@
 #include <stdio.h>
 
 /**
- * puts_half - prints the secnd half of a string
- * @str: the string ti print
+ * puts_half - prints the second
+ * half of a string
+ * @str: the string to print
  */
 void puts_half(char *str)
 {
 	int len = 0, i, n;
 
-	/*
-	 * obtain the length of the string
-	 */
 	while (*(str + len) != '\0')
 		len++;
 
-	/*
-	 * calculate the number of characters to print
-	 */
 	if (len % 2 == 0)
 		n = len / 2;
 	else
 		n = (len - 1) / 2;
 
-	/*
-	 * print the charcters
-	 */
 	for (i = n; i < len; i++)
 		_putchar(*(str + i));
 	_putchar('\n');
