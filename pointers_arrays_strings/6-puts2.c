2f6edb804c3ef7b65e52120f553b5c8c89250f54
lorecarreno <rodelugaliza@gmail.com>
2023-02-27 17:41:55 -0800

    pointers_arrays_strings

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -8,10 +8,16 @@
 void puts2(char *str)
 {
 	int i;
+	int j = 0;
+
+	while (str[j] != '\0')
+	{
+	j++;
+	}
 
 	for (i = 0; str[i] != '\0'; i += 2)
 	{
-		_putchar(str[i]);
+	_putchar(str[i]);
 	}
 
 	_putchar('n');
