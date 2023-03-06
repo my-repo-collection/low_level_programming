09809001612927321e10e073dee819c8ef8197c9
Lorenacg <rodelugaliza@gmail.com>
2023-03-06 00:48:15 -0300

    recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -5,7 +5,7 @@
  * _puts_recursion - imprime una cadena de caracteres,
  * seguida de un salto de linea
  * @s: string to be printed
- *
+ *i
  * Return: void
  */
 void _puts_recursion(char *s)
@@ -15,7 +15,7 @@ void _puts_recursion(char *s)
 		putchar('\n');
 		return;
 	}
-	
+
 	putchar(*s);
 	_puts_recursion(s + 1);
 }
