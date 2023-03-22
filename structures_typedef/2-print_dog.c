1d4378c23363ce791fd0b86fb98f99753c3b70b5
Lorenacg <rodelugaliza@gmail.com>
2023-03-22 06:51:50 -0300

    structures_typedef

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -4,14 +4,14 @@
 /**
  * print_dog - Prints a dog struct
  * @d: Pointer to struct dog
- * 
+ *
  * Return: void
  */
 void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	
+
 	printf("Name: %s\n", d->name ? d->name : "(nil)");
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
