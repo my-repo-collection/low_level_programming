9f626467ac820ead0dc5bc977546e68c8b8ae609
Lorenacg <rodelugaliza@gmail.com>
2023-03-22 06:26:14 -0300

    structures_typedef

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -10,11 +10,13 @@
  * Description: a struct that contains basic information about a dog
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /* DOG_H */
